57014f0335cc2adf6f37852a5ee2faf77172dfa9
gebeto <slavik.nychkalo@gmail.com>
2022-02-19 22:22:08 +0200

    add macbook template

---
+++ src/components/ImageCanvas/templates/macbook_pro_16_1/index.ts
@@ -0,0 +1,10 @@
+import templateImage from './template.png';
+
+
+export default {
+  title: 'MacBook Pro 16 1',
+  link: templateImage,
+  position: [406, 605],
+  size: [1236, 760],
+  cornerRadius: 12
+}
